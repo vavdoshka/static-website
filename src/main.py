8b5d77ef4b77f0f5431094d473e292d8f3e04efc
Vladimir Avdoshka <vova.avdoshka@gmail.com>
2024-11-10 17:33:00 +0100

    moving files around

--- src/main.py
+++ src/main.py
@@ -1,6 +1,34 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+BASE_DIRECTORY = os.path.dirname(__file__)
+STATIC_ASSETS = os.path.join(BASE_DIRECTORY, "..", "static")
+PUBLIC_ASSETS = os.path.join(BASE_DIRECTORY, "..", "public")
+
+def recursive_copy(filename, parent=""):
+    src = os.path.join(STATIC_ASSETS, parent, filename)
+    dest = os.path.join(PUBLIC_ASSETS, parent, filename)
+    if os.path.isfile(src):
+        print(f"Copy '{src}' to '{dest}'")
+        shutil.copy(src, dest)
+    elif os.path.isdir(src):
+        print(f"Creating directory at {dest}")
+        os.mkdir(dest)
+        ls = os.listdir(os.path.join(STATIC_ASSETS, parent, filename))
+        for fn in ls:
+            recursive_copy(fn, parent=os.path.join(parent, filename))
+
+
+
+def copy_from_static_to_public():
+
+    shutil.rmtree(PUBLIC_ASSETS, ignore_errors=True)
+    os.mkdir(PUBLIC_ASSETS)
+
+    for filename in os.listdir(STATIC_ASSETS):
+        recursive_copy(filename)
+
 def main():
-    text_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(text_node)
+    copy_from_static_to_public()
 
 main()
